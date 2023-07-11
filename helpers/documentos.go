90c8561d5769afcd661c6f7acbd832c5cd47c314
a52290451 <cuchitrin555@hotmail.com>
2023-07-10 22:22:25 -0500

    fix: corrección de archivos go.mod y go.sum

--- helpers/documentos.go
+++ helpers/documentos.go
@@ -140,43 +140,42 @@ func CertificacionVistoBueno(dependencia string, mes string, anio string) (perso
 					for _, actaInicio := range actasInicio {
 						if (actaInicio.FechaInicio.Year() == actaInicio.FechaFin.Year() && int(actaInicio.FechaInicio.Month()) <= mes_cer && actaInicio.FechaInicio.Year() <= anio_cer && int(actaInicio.FechaFin.Month()) >= mes_cer && actaInicio.FechaFin.Year() >= anio_cer) ||
 							(actaInicio.FechaInicio.Year() < actaInicio.FechaFin.Year() && int(actaInicio.FechaInicio.Month()) <= mes_cer && actaInicio.FechaInicio.Year() <= anio_cer && int(actaInicio.FechaFin.Month()) <= mes_cer && actaInicio.FechaFin.Year() > anio_cer) ||
-							(actaInicio.FechaInicio.Year() < actaInicio.FechaFin.Year() && int(actaInicio.FechaInicio.Month()) >= mes_cer && actaInicio.FechaInicio.Year() < anio_cer && int(actaInicio.FechaFin.Month()) >= mes_cer && actaInicio.FechaFin.Year() >= anio_cer){
-								if err := GetRequestNew("CumplidosDveUrlParametros", "parametro/?query=CodigoAbreviacion.in:PAD_DVE|AD_DVE|AP_DVE", &parametros); err == nil{
-									for _, parametro := range parametros{
-										if resultado == false && imprimido == false{
-											if err := GetRequestNew("CumplidosDveUrlCrud", "pago_mensual/?query=EstadoPagoMensualId:" + strconv.Itoa(parametro.Id) + ",NumeroContrato:" + vinculacion_docente.NumeroContrato + ",VigenciaContrato:" + strconv.FormatInt(vinculacion_docente.Vigencia, 10) + ",Mes:" + mes + ",Ano:" + anio,  &pagos_mensuales); err == nil{
-												if len((pagos_mensuales)) == 0 {
-													imprimido = true
-													if err := GetRequestLegacy("CumplidosDveUrlCrudAgora", "informacion_proveedor/?query=NumDocumento:" + strconv.Itoa(vinculacion_docente.PersonaId), &contratistas); err == nil{
-														for  _, contratista := range contratistas{
-															persona.NumDocumento = contratista.NumDocumento
-															persona.Nombre = contratista.NomProveedor
-															persona.NumeroContrato = actaInicio.NumeroContrato
-															persona.Vigencia = actaInicio.Vigencia
-															personas = append(personas, persona)
-														}
-													}else{
-														panic(err.Error())
+							(actaInicio.FechaInicio.Year() < actaInicio.FechaFin.Year() && int(actaInicio.FechaInicio.Month()) >= mes_cer && actaInicio.FechaInicio.Year() < anio_cer && int(actaInicio.FechaFin.Month()) >= mes_cer && actaInicio.FechaFin.Year() >= anio_cer) {
+							if err := GetRequestNew("CumplidosDveUrlParametros", "parametro/?query=CodigoAbreviacion.in:PAD_DVE|AD_DVE|AP_DVE", &parametros); err == nil {
+								for _, parametro := range parametros {
+									if resultado == false && imprimido == false {
+										if err := GetRequestNew("CumplidosDveUrlCrud", "pago_mensual/?query=EstadoPagoMensualId:"+strconv.Itoa(parametro.Id)+",NumeroContrato:"+vinculacion_docente.NumeroContrato+",VigenciaContrato:"+strconv.FormatInt(vinculacion_docente.Vigencia, 10)+",Mes:"+mes+",Ano:"+anio, &pagos_mensuales); err == nil {
+											if len((pagos_mensuales)) == 0 {
+												imprimido = true
+												if err := GetRequestLegacy("CumplidosDveUrlCrudAgora", "informacion_proveedor/?query=NumDocumento:"+strconv.Itoa(vinculacion_docente.PersonaId), &contratistas); err == nil {
+													for _, contratista := range contratistas {
+														persona.NumDocumento = contratista.NumDocumento
+														persona.Nombre = contratista.NomProveedor
+														persona.NumeroContrato = actaInicio.NumeroContrato
+														persona.Vigencia = actaInicio.Vigencia
+														personas = append(personas, persona)
 													}
-												}else{
-													resultado = true
+												} else {
+													panic(err.Error())
 												}
+											} else {
 												resultado = true
 											}
-										} else {
-											panic(err.Error())
+											resultado = true
 										}
+									} else {
+										panic(err.Error())
 									}
-									resultado = false
-									imprimido = false
 								}
 								resultado = false
+								imprimido = false
 							}
+							resultado = false
 						}
 					}
-				} else {
-					panic(err.Error())
 				}
+			} else {
+				panic(err.Error())
 			}
 		}
 	} else {
